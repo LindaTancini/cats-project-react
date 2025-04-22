1a581d9f39aaa5f81ba4d1675bf691bc6d8c758f
LindaTancini <lindatancini95@gmail.com>
2025-04-22 14:43:38 +0200

    feat: add button second task

--- src/components/CatImage.jsx
+++ src/components/CatImage.jsx
@@ -1,23 +1,29 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 function CatImage() {
   const [imageUrl, setImageUrl] = useState("");
 
-  useEffect(() => {
+  // CREO FUNZIONE PER CARICARE L'IMMAGINE
+  const changeImage = () => {
     axios
       .get("https://api.thecatapi.com/v1/images/search")
-      .then((response) => {
-        setImageUrl(response.data[0].url);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      .then((response) => setImageUrl(response.data[0].url))
+      .catch((error) =>
+        console.error("Errore nel recupero dell'immagine:", error)
+      );
+  };
+
+  // CARICO UN'IMMAGINE CASUALE
+  useEffect(() => {
+    changeImage();
   }, []);
+
   return (
     <div>
       <h4>Genera immagine di un gatto casuale:</h4>
       {imageUrl && <img src={imageUrl} alt="Gatto Casuale" />}
+      <button onClick={changeImage}>Cambia immagine</button>
     </div>
   );
 }

85bcf0b6f1ec89a6c2165b062d35e8795875715d
LindaTancini <lindatancini95@gmail.com>
2025-04-22 15:41:59 +0200

    feat: add sixth task

---
+++ src/components/CatShelter.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+function CatShelter() {
+  const [numeroGatti, setNumeroGatti] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newCats = Math.floor(Math.random() * 3) + 1;
+      setNumeroGatti((prev) => prev + newCats);
+    }, 5000);
+
+    // PULIZIA DELL'INTERVALLO
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h4>Gatti attualmente nel rifugio:</h4>
+      <p>{numeroGatti}</p>
+    </div>
+  );
+}
+
+export default CatShelter;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,14 @@ import CatColorFavorite from "./CatColorFavorite";
 import CatCounter from "./CatCounter";
 import CatFact from "./CatFact";
 import CatImage from "./CatImage";
+import CatShelter from "./CatShelter";
 
 function Main() {
   return (
     <>
       <h2>Gatti, gatti ovunque ♥ </h2>
       <CatCounter />
+      <CatShelter />
       <CatImage />
       <CatFact />
       <CatColorFavorite />

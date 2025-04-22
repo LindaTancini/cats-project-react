50094e006fbab7a1b8f0898a195ce989518b49d0
LindaTancini <lindatancini95@gmail.com>
2025-04-22 14:59:55 +0200

    feat: change text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ import CatImage from "./CatImage";
 function Main() {
   return (
     <>
-      <h2>Contenuto delle task ♥ </h2>
+      <h2>Gatti, gatti ovunque ♥ </h2>
       <CatCounter />
       <CatImage />
       <CatFact />
